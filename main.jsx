96ba81b0f13325df9f95dc0db135ed60a812f023
Mohammad <Mohammadrezaaalaei@gmail.com>
2023-02-08 23:11:52 +0330

    Adding dist

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'tachyons'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='tc ma0 pa0'>
+      <App />
+    </div>
+    
+  </React.StrictMode>,
+)
